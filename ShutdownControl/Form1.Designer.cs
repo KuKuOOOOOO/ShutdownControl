7bfcbef3ced881f4344dd7e5ac94ffe57c810946
Fate <a981211661@gmail.com>
2022-03-01 01:36:10 +0800

    Version 1.0
    Signed-off-by: Fate <a981211661@gmail.com>

---
+++ ShutdownControl/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace ShutdownControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.mnuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Calibri", 24F);
+            this.button1.Location = new System.Drawing.Point(97, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 102);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Timer Start";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(95, 176);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 27);
+            this.label1.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "ShutdownControl";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.BalloonTipClicked += new System.EventHandler(this.notifyIcon1_BalloonTipClicked);
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuExit});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(115, 26);
+            // 
+            // mnuExit
+            // 
+            this.mnuExit.Name = "mnuExit";
+            this.mnuExit.Size = new System.Drawing.Size(114, 22);
+            this.mnuExit.Text = "結束(&X)";
+            this.mnuExit.Click += new System.EventHandler(this.mnuExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 268);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "ShutdownControl";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmTrayIcon);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mnuExit;
+    }
+}
+
---
+++ ShutdownControl/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace ShutdownControl
+{
+    public partial class Form1 : Form
+    {
+        private static int count = 1;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        //Button按下
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            timer1.Interval = 1000;//count 1秒
+            timer1.Enabled = true; //開啟timer1
+            label1.Text = "The computer will power off at" + DateTime.Now.AddHours(1).ToString("HH:mm:ss");//顯示再多久時間會關機
+
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            count++;
+            if (count == 3590)//剩10秒時最後提示
+                label1.Text = "The computer will power off...";
+            if (count > 3600)//1hr過後會關機
+            {
+
+                System.Diagnostics.Process.Start("C:\\WINDOWS\\system32\\shutdown.exe", "-f -s -t 0");
+                timer1.Enabled = false;
+
+            }
+        }
+
+        //Icon滑鼠雙點擊showForm
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            ShowForm();
+        }
+        //按下顯示提示時showForm
+        private void notifyIcon1_BalloonTipClicked(object sender, EventArgs e)
+        {
+            ShowForm();
+        }
+        //顯示視窗程式
+        private void ShowForm()
+        {
+            if (this.WindowState == FormWindowState.Minimized) 
+            {
+                //如果目前是縮小狀態 才要回復成一般大小的視窗
+                this.Show();
+                this.WindowState = FormWindowState.Normal;
+            }
+
+            //Activate the form
+            this.Activate();
+            this.Focus();
+        }
+        //Menu選單:結束
+        private void mnuExit_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+            Close();
+        }
+
+        /// <summary>
+        /// 使用者按下視窗關閉，把它最小化就好
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void frmTrayIcon(object sender, FormClosingEventArgs e)
+        {
+            if (this.WindowState != FormWindowState.Minimized)
+            {
+                e.Cancel = true;
+                this.WindowState = FormWindowState.Minimized;//將視窗最小化
+                notifyIcon1.Tag = string.Empty;
+                this.notifyIcon1.Visible = true;            //Icon設定隱藏
+                this.Hide();                                //視窗隱藏
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
